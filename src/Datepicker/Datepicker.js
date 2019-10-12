5ec3a0baadd8ea2dd5763af9065f118a30274709
dondrich <dbo1093@gmail.com>
2019-10-12 22:52:46 +0300

    added label for datepicker component

--- src/Datepicker/Datepicker.js
+++ src/Datepicker/Datepicker.js
@@ -3,10 +3,11 @@ import React from "react";
 import "./Datepicker.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 
-const Datepicker = () => {
+const Datepicker = ({ label, labelPosition="left" }) => {
   return (
-    <div className="datepicker-component">
-      <input type="date" className="datepicker" />
+    <div className={`datepicker-component datepicker-component-${labelPosition}`}>
+      <label className="datepicker-label" htmlFor="datepicker">{label}</label>
+      <input type="date" id="datepicker" />
     </div>
   );
 };
