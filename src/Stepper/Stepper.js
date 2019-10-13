cfab83a582957229349eee5415b7d902deecb0da
dondrich <dbo1093@gmail.com>
2019-10-13 15:53:48 +0300

    Stepper files

---
+++ src/Stepper/Stepper.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Stepper.css";
+
+const Stepper = props => {
+  let { steps, currentStep } = props;
+
+  const items = Array.apply(null, Array(steps)).map((_, i) => {
+    return (
+      <div className="step-container" key={i.toString()}  >
+        <span className={i <= currentStep ? "step-number complete" :"step-number"}>{i+1}</span>
+        {i+1 === steps ? '' : <div className={i+1 <= currentStep ? "separator complete" : "separator"}></div>}
+      </div>
+      )
+  });
+
+  return (
+    <div className="stepper-container">
+      {items}
+    </div>
+  )
+};
+
+Stepper.defaultProps = {
+  steps: 3,
+  currentStep: 0,
+};
+
+Stepper.propTypes = {
+  steps: PropTypes.number,
+  currentStep: PropTypes.number,
+};
+
+export default Stepper;
